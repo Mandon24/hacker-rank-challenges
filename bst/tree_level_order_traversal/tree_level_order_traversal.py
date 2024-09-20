99ac7893e9077b9c93bedf2b191eb22845552235
Michael Andon <andon.m3@gmail.com>
2024-09-19 17:49:38 -0700

    feat: bst challenges for height and level order traversal

---
+++ bst/tree_level_order_traversal/tree_level_order_traversal.py
@@ -0,0 +1,71 @@
+class Node:
+    def __init__(self, info): 
+        self.info = info  
+        self.left = None  
+        self.right = None 
+        self.level = None 
+
+    def __str__(self):
+        return str(self.info) 
+
+class BinarySearchTree:
+    def __init__(self): 
+        self.root = None
+
+    def create(self, val):  
+        if self.root == None:
+            self.root = Node(val)
+        else:
+            current = self.root
+         
+            while True:
+                if val < current.info:
+                    if current.left:
+                        current = current.left
+                    else:
+                        current.left = Node(val)
+                        break
+                elif val > current.info:
+                    if current.right:
+                        current = current.right
+                    else:
+                        current.right = Node(val)
+                        break
+                else:
+                    break
+from collections import deque
+
+
+"""
+Node is defined as
+self.left (the left child of the node)
+self.right (the right child of the node)
+self.info (the value of the node)
+"""
+
+def levelOrder(root):
+    if not root:
+        return
+     
+    bst_queue = deque()
+    bst_queue.append(root)
+
+    while len(bst_queue) > 0:
+        node = bst_queue.popleft()
+        print(node.info, end=" ")
+        if node.left:
+            bst_queue.append(node.left)
+        if node.right:
+            bst_queue.append(node.right)
+
+
+
+tree = BinarySearchTree()
+t = int(input())
+
+arr = list(map(int, input().split()))
+
+for i in range(t):
+    tree.create(arr[i])
+
+levelOrder(tree.root)
